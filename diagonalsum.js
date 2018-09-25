09619debf06bceb73865f1eaeea3db370c9905aa
Adam Bezulski <abezulski@hotmail.com>
2018-09-25 23:02:02 +0100

    added diagonalsum solutions

---
+++ diagonalsum.js
@@ -0,0 +1,27 @@
+/*
+    Find sum of top-left to bottom-right diagonals
+    http://www.codewars.com/kata/find-sum-of-top-left-to-bottom-right-diagonals/javascript
+
+    Given a "square" array of subarrays, find the sum of values from the first value of the first array, the second value of the second array, the third value of the third array, and so on...
+
+    Example 1:
+
+    var exampleArray = [[1, 0, 0, 0],
+                    [0, 1, 0, 0],
+                    [0, 0, 1, 0],
+                    [0, 0, 0, 1]]
+
+    diagonalSum(exampleArray) // => 4
+*/
+
+function diagonalSum(arr) {
+    let result = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        result += arr[i][i];
+    }
+
+    return result;
+}
+
+export default diagonalSum;
---
+++ diagonalsum.v2.js
@@ -0,0 +1,21 @@
+/*
+    Find sum of top-left to bottom-right diagonals
+    http://www.codewars.com/kata/find-sum-of-top-left-to-bottom-right-diagonals/javascript
+
+    Given a "square" array of subarrays, find the sum of values from the first value of the first array, the second value of the second array, the third value of the third array, and so on...
+
+    Example 1:
+
+    var exampleArray = [[1, 0, 0, 0],
+                    [0, 1, 0, 0],
+                    [0, 0, 1, 0],
+                    [0, 0, 0, 1]]
+
+    diagonalSum(exampleArray) // => 4
+*/
+
+function diagonalSum(arr) {
+    return arr.reduce((acc, v, i) => { return acc + v[i] }, 0);
+}
+
+export default diagonalSum;
